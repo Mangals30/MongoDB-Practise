d292debe9c9f4f2211b957848e10749a69897547
Mangals30 <mangalam301185@gmail.com>
2020-03-25 01:22:24 +0200

    ejs changes

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8080
 let {students} = require('./views/students')
 const fs = require('fs')
 const os = require('os')
 const {dateToday} = require('./views/date')
 const bodyParser = require('body-parser')
+const ejs = require('ejs')
 
 app.use(express.static('public'))
 app.use(bodyParser.json())
-
+app.set('view engine', 'ejs')
 app.use((req,res,next) => {
     const user = os.hostname
     const page = req.url
@@ -24,9 +25,11 @@ app.use((req,res,next) => {
 })
 
 app.get('/', (req,res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.render('index')
 })
-
+app.get('/students', (req,res) => {
+    res.render('students', {students})
+  })
 app.get('/about', (req,res) => {
     res.sendFile(__dirname + '/views/about.html')
   })
@@ -36,7 +39,7 @@ app.get('/about', (req,res) => {
   app.get('/text', (req,res) => {
     res.sendFile(__dirname + '/views/text.txt')
   })
-  app.get('/students', (req,res) => {
+  app.get('/students/api', (req,res) => {
       if(students.length > 0) {
         res.send(students)
       }
@@ -44,7 +47,7 @@ app.get('/about', (req,res) => {
           res.send('No students exist in the list')
       }
   })
-  app.get('/students/:id',(req,res) => {
+  app.get('/students/api/:id',(req,res) => {
       const id = req.params.id
       const student = students.find(student => student.id == id || student.firstName.toLowerCase() == id.toLowerCase())
       if(student) {
@@ -54,7 +57,7 @@ app.get('/about', (req,res) => {
        res.send('Student does not exist') 
      } 
   })
-  app.post('/students',(req,res) => {
+  app.post('/students/api',(req,res) => {
       const allIds = students.map(student => student.id)
       let maxId = Math.max(...allIds)
       const id = maxId+1
@@ -62,7 +65,7 @@ app.get('/about', (req,res) => {
       students.push(req.body)
       res.send('A data has been added')
   }) 
-  app.put('/students/:id',(req,res) => {
+  app.put('/students/api/:id',(req,res) => {
       let id = req.params.id
       const editedStudent = students.filter(student => student.id == id)
       if(editedStudent.length) {
@@ -80,7 +83,7 @@ app.get('/about', (req,res) => {
           res.send('Student doesnot exist to edit')
       }
   })
-  app.delete('/students/:id',(req,res) => {
+  app.delete('/students/api/:id',(req,res) => {
       const id = req.params.id
       const deletedStudent = students.filter(student => student.id == id)
       if(deletedStudent.length)
